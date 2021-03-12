3f82d702d993f37ea59e24247ffc12cd30349da2
FNigrok <58119929+FNigrok@users.noreply.github.com>
2021-03-12 23:49:44 +0300

    Add files via upload

---
+++ MARCH13/Menu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private int currentMenu = 0;
+    public GameObject[] menu;
+
+    public void NextFrame(int changeMenu)
+    {
+        menu[currentMenu].SetActive(false);
+        menu[changeMenu].SetActive(true);
+        currentMenu = changeMenu;
+    }
+
+}
